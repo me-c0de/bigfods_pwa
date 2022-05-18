e243cea99df80613341dc5c0f383c040774aa401
Christopher Meyer <christopher_meyer@outlook.de>
2022-05-18 16:39:58 +0200

    feature: add new edit component

---
+++ src/app/presentation/edit/edit.component.ts
@@ -0,0 +1,45 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Cat} from '../../cat';
+import {HttpClient} from '@angular/common/http';
+import {CatService} from '../../cat.service';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.scss']
+})
+export class EditComponent implements OnInit {
+
+  selectedFile: File;
+  @Input() cat: Cat;
+
+  constructor(private http: HttpClient, private catService: CatService) { }
+
+  ngOnInit(): void {
+  }
+
+
+  public onFileChanged(event): void {
+    this.selectedFile = event.target.files[0];
+  }
+
+
+  public onUpdate(name: string, description: string): void {
+
+    const formdata = new FormData();
+    formdata.append('image', this.selectedFile, this.selectedFile.name);
+    formdata.append('filename', this.selectedFile.name);
+
+    name = name.trim();
+    if (!name){
+      return;
+    }
+
+    const id = this.cat.id;
+
+    this.catService.updateCat({ id, name, description} as Cat)
+      .subscribe( cat => {
+        this.http.put('http://127.0.0.1:8080/api/images/' + cat.image.id, formdata).subscribe(res => console.log(res));
+      });
+  }
+}
