05696c172d9e3d2a8e96ab178a41b7039c7d5385
Christopher Meyer <christopher_meyer@outlook.de>
2022-05-19 12:24:56 +0200

    feature: create authentication service with related interceptor

---
+++ src/app/service/interceptor/token.interceptor.ts
@@ -0,0 +1,27 @@
+import {Injectable, Injector} from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AuthService} from '../auth/auth.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(private inject: Injector) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const authservice = this.inject.get(AuthService);
+
+    const jwtToken = request.clone({
+      setHeaders: {
+        Authorization: 'bearer ' + authservice.getToken()
+      }
+    });
+
+    return next.handle(jwtToken);
+  }
+}
